1f8036cecf724a6ded8329d5b1e8c68110af22f7
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-05-18 01:40:07 +0200

    Create the API client

---
+++ src/services/apiClient.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const baseURL = "https://wookie.codesubmit.io";
+
+const headers = {
+  common: {
+    Authorization: `Bearer Wookie2019`,
+  },
+};
+
+const api = axios.create({
+  baseURL: baseURL,
+  timeout: 0,
+  headers: headers,
+});
+
+api.interceptors.response.use(undefined, (err) => {
+  if (!err.response || err.response?.status !== 200) {
+    throw Error("Failed to communicate with the API. Please, try again.");
+  }
+  throw error.response;
+});
+
+export { api };
