81e0d826d52dc6d72b94aa65c6a9fb468763d306
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-05-19 05:02:59 +0200

    Use movie slug for routing

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Poster from "../components/Poster";
+import { useParams } from "react-router-dom";
 
 const Details = () => {
+  const { slug } = useParams();
   const state = useSelector((state) => state.movies);
-  const movie = state?.movies[0]?.movies[0];
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    state.movies.map((moviesByGenre) =>
+      moviesByGenre.movies.filter(
+        (movie) => movie.slug === slug && setMovie(movie)
+      )
+    );
+  }, [slug, state]);
 
   return (
     <div className="container">
