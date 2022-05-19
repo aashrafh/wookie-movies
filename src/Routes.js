2a950103c18328253df7719e6f3a57c7c2feb8af
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-05-19 04:57:18 +0200

    Create not found page

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
+import NotFound from "./pages/NotFound";
 
 const MovieRoutes = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/detail/:slug" element={<Details />} />
-      <Route path="*" element={<h1>Not Found</h1>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="not_found">
+      <p>Sorry, we can't find that page.</p>
+    </div>
+  );
+};
+export default NotFound;
